c291ac51204d48faa40a8cb1a1f59e0ba6e10537
Frank Denis <github@pureftpd.org>
2020-05-20 18:25:57 +0200

    Make Error look like a standard error

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,22 @@
+use std::fmt::{self, Display};
+
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum Error {
     KeyTooLong,
     TagMismatch,
     InvalidLength,
     KeyRequired,
+}
+
+impl std::error::Error for Error {}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::KeyTooLong => write!(f, "Key too long"),
+            Error::TagMismatch => write!(f, "Tag mismatch"),
+            Error::InvalidLength => write!(f, "Invalid length"),
+            Error::KeyRequired => write!(f, "A key is required"),
+        }
+    }
 }
