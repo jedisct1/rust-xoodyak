868bb8892df7b9e5fa93fe30320ca81ed5bb4556
Frank Denis <github@pureftpd.org>
2022-10-10 18:18:54 +0200

    Format

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -26,7 +26,7 @@ fn main() {
     {
         let mut out = [0u8; 64];
         let mut st = XoodyakHash::new();
-        let res = bench.run(options,  || {   
+        let res = bench.run(options,  || {
             st.absorb(b"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ");
             st.squeeze(&mut out);
             out
@@ -35,10 +35,10 @@ fn main() {
     }
 
     {
-      let mut out = [0u8; 64];
-        let mut st = XoodyakKeyed::new(b"key", None,None, None).unwrap();
-        let res = bench.run(options,  || {   
-          st.absorb(b"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ");
+        let mut out = [0u8; 64];
+        let mut st = XoodyakKeyed::new(b"key", None, None, None).unwrap();
+        let res = bench.run(options,  || {
+            st.absorb(b"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ");
             st.squeeze(&mut out);
             out
         });
